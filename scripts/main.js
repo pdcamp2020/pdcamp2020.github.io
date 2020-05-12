2edce0a97999acc94a3587b49338b256a52e167b
Ana Moreno <amoreno5@uncc.edu>
2020-05-12 00:13:38 -0400

    jpeg fix

--- scripts/main.js
+++ scripts/main.js
@@ -87,7 +87,7 @@ function convertCSVArrayToTraineeData(csvArrays) {
     trainee.id = parseInt(traineeArray[5]) - 1; // trainee id is the original ordering of the trainees in the first csv
     trainee.grade="a"; //all girls get color border a
     trainee.image =
-      trainee.name_romanized.replace(" ", "").replace("-", "") + ".jpg";
+      trainee.name_romanized.replace(" ", "").replace("-", "") + ".jpeg";
     return trainee;
   });
   filteredTrainees = trainees;
